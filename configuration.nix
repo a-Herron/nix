84b9b8504c0be8aa4f54ec98948172ef9c8a4b96
a-Herron <austinherronmt+git@gmail.com>
2024-07-06 13:25:35 -0600

    Setup volume

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alsa-utils
+    pavucontrol
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
--- home.nix
+++ home.nix
@@ -28,6 +28,7 @@
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
+    pavucontrol
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
